41f240a8d39696ded392f2f99073451bf7f54956
Mathieu-Heuzey <mathieu.heuzey@gmail.com>
2016-03-04 11:46:52 +0100

    Ajout du marker de l'utilisateur

--- Homeless/app/src/main/java/mobile/homeless/MapsActivity.java
+++ Homeless/app/src/main/java/mobile/homeless/MapsActivity.java
@@ -2,6 +2,10 @@ package mobile.homeless;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.location.Location;
+import android.location.LocationManager;
+import android.util.Log;
+import android.content.Context;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,6 +13,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.Console;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -38,9 +44,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+        System.out.println(longitude);
+        System.out.println(latitude);
+        LatLng mLatLng = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(mLatLng).title("My Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mLatLng));
+
+        
+        //requete API
+        // REcupere toutes les personnes
+        // Je parse ->
     }
 }
